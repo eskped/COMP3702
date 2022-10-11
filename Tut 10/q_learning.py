075ae8ad79a7b43ed124120a68719f288093d7c6
Eskil <eskped@gmail.com>
2022-10-11 14:18:50 +1000

    Added the opportinty to approximate the values (Q)

--- Tut 10/q_learning.py
+++ Tut 10/q_learning.py
@@ -1,16 +1,21 @@
 import gym
 import numpy as np
+import random
 
 env = gym.make('FrozenLake-v1', desc=None, map_name='4x4', is_slippery=True)
 
 
-n_episode = 1000
+n_episode = 10000
 max_timestep_per_episode = 100
 
 alpha = 0.1
 epsilon = 0.1
 gamma = 0.99
+n_features = 10
+w = np.random.rand(n_features)
+
 Q = np.random.rand(env.observation_space.n, env.action_space.n)
+# Q = np.zeros((env.observation_space.n, env.action_space.n))
 Q[-1, :] = np.zeros(env.action_space.n)
 
 print(Q)
@@ -18,9 +23,13 @@ print(Q)
 
 def epsilon_greedy(Q, s, epsilon):
     if np.random.rand() < epsilon:
-        return np.random.randint(env.action_space.n)
+        return np.random.choice(env.action_space.n)
     else:
         return np.argmax(Q[s, :])
+
+
+def get_feature(s, a):
+    return
 
 
 for ep in range(n_episode):
@@ -30,9 +39,14 @@ for ep in range(n_episode):
         a = epsilon_greedy(Q, s, epsilon)
         s_next, r, terminated, _, _ = env.step(a)
 
-        # print(
-        #     f'ep: {ep}, t: {t}, s: {s}, a: {a}, r: {r}, s_next: {s_next}, terminated: {terminated}')
+        # tabular Q-learning update
         Q[s, a] += alpha * (r + gamma*np.max(Q[s_next, :]) - Q[s, a])
+        # w = w + alpha * (r + gamma * np.max(Q_approx[s_next, :]) - Q[s, a])*Q[s,a] # linear Q-learning update
+
+        def Q_approx(s, a):
+            f_sa = get_feature(s, a)  # 3-element vector
+            value = np.dot(w, f_sa)
+            return value
 
         s = s_next
         if terminated:
@@ -43,4 +57,4 @@ policy = np.argmax(Q, axis=1)
 for s in range(env.observation_space.n):
     policy[s] = np.argmax(Q[s, :])
 
-print(policy)
+print(f'policy: {policy}')
